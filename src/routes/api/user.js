5781b384e1348388e7aa97dc42f05cf67e4d2eaa
Kayode Gabriel <kayodegabrielA@gmail.com>
2019-11-15 10:23:00 +0100

    fixed bugs

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -3,15 +3,14 @@ import { createEmp, logIn } from '../../controllers/user';
 import validator from '../../middlewares/validateSchema';
 import { verifyToken, checkAdmin } from '../../middlewares/jwtToken';
 
-const { signInValidator, createUserValidator } = validator;
+// const { signInValidator, createUserValidator } = validator;
 
 const router = Router();
-router.post('/signin', signInValidator, logIn);
+router.post('/signin', logIn);
 router.post(
   '/create-user',
   verifyToken,
   checkAdmin,
-  createUserValidator,
   createEmp
 );
 
