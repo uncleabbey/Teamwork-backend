44b89389cd532bbd89fe176b8bf870b0c5a39574
Kayode Gabriel <kayodegabrielA@gmail.com>
2019-11-26 14:23:57 +0100

    fixed bg-feed-query-bug

--- src/controllers/gifComment.js
+++ src/controllers/gifComment.js
@@ -1,7 +1,9 @@
 import comments from '../services/gifComments';
 import gifs from '../services/gifs';
+import users from '../db/models/user';
 
 const { getGif } = gifs;
+const { findUserById } = users;
 const { createGifComments } = comments;
 const createGifsComment = async (req, res) => {
   const { userId } = req.decoded;
@@ -10,13 +12,21 @@ const createGifsComment = async (req, res) => {
   try {
     const data = await createGifComments(gifId, userId, comment);
     const gif = await getGif(gifId);
+    const user = await findUserById(userId);
     return res.status(201).json({
       status: 'success',
       data: {
         message: 'comment successfully created',
         createdOn: data.created_on,
         gifTitle: gif.title,
-        comment: data.comment
+        comment: data.comment,
+        commentData: {
+          commentId: data.comment_id,
+          createdOn: data.created_on,
+          comment: data.comment,
+          firstName: user.first_name,
+          lastName: user.last_name
+        }
       }
     });
   } catch (error) {
